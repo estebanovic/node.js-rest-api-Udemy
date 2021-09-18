0754c4d43d5d6a352e26319dfe69b4e3eb0bcd77
Esteban Obilinovic <e.obilinovic@gmail.com>
2021-09-18 03:15:04 -0300

    structure

---
+++ controllers/users.controller.js
@@ -0,0 +1,20 @@
+const usersService = require("../services/users.service")
+
+exports.register = (req, res, next) => {
+    const data = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        emailId: req.body.emailId,
+        password: req.body.password
+    };
+    usersService.register(data, (err, result) => {
+        if (err) {
+            console.log(err);
+            return res.status(400).send({ success: 0, data: "Bad request" });
+        }
+        return res.status(201).send({
+            success: 1,
+            data: result
+        });
+    });
+};
--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require("express");
 
 const app = express();
+const usersRoute = require("./routes/users.route")
 
-app.use("/test", (req, res) => {
-    console.log("Reiceved request")
-    res.status(200).send("success");
-})
+app.use("/users", usersRoute)
+
 
 app.listen(3000, () => {
     console.log("I am ready to listen you");
